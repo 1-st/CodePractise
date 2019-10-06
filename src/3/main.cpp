4b9acbe484bedfe0f6f415eff06e18c1377f6bab
UsrAlpha <twx19975261083@gmail.com>
2019-10-06 16:27:08 +0800

    finish the src/3

--- src/3/main.cpp
+++ src/3/main.cpp
@@ -1,19 +1,19 @@
 #include <DC/utils/table.hpp>
 #include <DC/macro/file.hpp>
 #include <DC/defer.hpp>
+#include <DC/utils/type.hpp>
 #include <vector>
 #include <iostream>
-
+#include <string>
+#include <iomanip>
+//求最长子串
 int main(){
 	F_IO;
 	std::vector<char>X,Y;
-	int count = 0;
 	auto * target = &X;
 	while(true){
 		char buf='E';
 		in.get(buf);
-		count++;
-		std::cout<<count<<":"<<buf<<" ";
 		if(in.eof())
 			break;
 		else if(buf=='\n'){
@@ -22,15 +22,30 @@ int main(){
 		}
 		(*target).push_back(buf);
 	}
-	std::cout<<"X:";
-	for(auto i = X.cbegin();i!=X.cend();i++){
-		std::cout<<*i<<" ";
+	table<int> len(X.size()+1,Y.size()+1);
+	table<std::string> hint(X.size()+1,Y.size()+1);
+	len.setValue(0);
+	hint.setValue(" ");
+	for(uint64 i = 1;i<X.size()+1;i++){
+		for(uint64 j = 1;j<Y.size()+1;j++){
+			if(X[i-1]==Y[j-1]){
+				len[i][j] = len[i-1][j-1]+1;
+				hint[i][j]="↖"; 
+			}else if(len[i-1][j]>=len[i][j-1]){
+				len[i][j] = len[i-1][j];
+				hint[i][j]="↑";
+			}else{
+				len[i][j] = len[i][j-1];
+				hint[i][j]="←";
+			}
+		}
 	}
-	std::cout<<std::endl;
-	std::cout<<"Y:";
-	for(auto i = Y.cbegin();i!=Y.cend();i++){
-		std::cout<<*i<<" ";
+	for(uint64 i=0;i<X.size()+1;i++){
+		for(uint64 j=0;j<Y.size()+1;j++){
+			out<<std::setw(3)<<len[i][j]
+			<<hint[i][j];
+		}
+		out<<std::endl<<std::endl;
 	}
-	std::cout<<std::endl;
 
 }
